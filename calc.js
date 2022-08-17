1e61fc59377c528c884d87734f02dbec78f425a6
gabewin <gabewin@protonmail.com>
2022-08-17 12:33:11 -0700

    completed button functionality

--- calc.js
+++ calc.js
@@ -1,38 +1,35 @@
-let num1 = 0;
+let num1 = '';
 let num2 = 0;
 let operation = '';
 let displayValue = '';
-let displayResults = '';
 
 function operate(operation, num1, num2){
     num1 = parseInt(num1);
     num2 = parseInt(num2);
     if(operation == 'add'){
-        return num1+num2;
+        return (num1+num2).toFixed(2);
     }
     else if(operation == 'sub'){
-        return num1-num2;
+        return (num1-num2).toFixed(2);
     }
     else if(operation == 'div'){
         if(num2 == 0){
             return 'nice try :)'
         }
-        return num1/num2;
+        return (num1/num2).toFixed(2);
     }
     else if(operation == 'mult'){
-        return num1*num2;
+        return (num1*num2).toFixed(2);
     }
 }
 
 function addNumberEventListeners(){
     const numContainer = document.querySelector('#numbers');
     const numbers = numContainer.getElementsByClassName('number');
-    console.log(numbers);
     for(let i = 0; i<numbers.length; i++){
         // and for each one we add a 'click' listener
         numbers[i].addEventListener('click', () => {
             displayValue = displayValue+numbers[i].id;
-            console.log(`${displayValue}`);
             document.getElementById('results').innerHTML = displayValue;
         });
       };
@@ -40,21 +37,29 @@ function addNumberEventListeners(){
 }
 
 function addOperationEventListeners(){
-    console.log("we're here in the function");
     const opContainer = document.querySelector('#functions');
     const operations = opContainer.getElementsByClassName('operation');
-    console.log(operations);
     for(let i = 0; i<operations.length; i++){
-        console.log('and now we\'re in the for loop');
         // and for each one we add a 'click' listener
         operations[i].addEventListener('click', () => {
+            
+        if(num1 == ''){
             num1 = displayValue;
             displayValue = '';
-            operation = displayValue+operations[i].id;
-            console.log(`${num1}${operation}`);
+            console.log(`${num1}`);
             document.getElementById('results').innerHTML = displayValue;
+            operation = operations[i].id;
+        } else {
+            num2 = displayValue;
+            let result = operate(operation, num1, num2);
+            operation = operations[i].id;
+            num1 = result;
+            displayValue = '';
+            console.log(`num1: ${num1} result:${result}`);
+            document.getElementById('results').innerHTML = displayValue;
+        }
         });
-      };
+    };
 
 }
 
@@ -62,9 +67,19 @@ function addOtherEventListeners(){
     eqButton = document.getElementById('eq');
     eqButton.addEventListener('click', () => {
         num2 = displayValue;
-        displayResults = operate(operation, num1, num2);
-        document.getElementById('results').innerHTML = displayResults;
-        num1 = displayValue;
+        let result = operate(operation, num1, num2);
+        operation = '';
+        num1= '';
+        num2 = 0;
+        displayValue = '';
+        document.getElementById('results').innerHTML = result;
+    });
+
+    clearButton = document.getElementById('cl');
+    clearButton.addEventListener('click', () => {
+        displayValue = '';
+        document.getElementById('results').innerHTML = displayValue;
+        num1 = '';
         num2 = 0;
         operation = '';
     });
